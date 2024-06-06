7998c4bf3b866e0241943b9fac71c3eb36645645
MohanKumarBT1997 <btmohankumar777@gmail.com>
2024-06-06 17:00:06 +0530

    Security Implemented

--- src/main/java/com/jsp/wm/repository/AdminRepository.java
+++ src/main/java/com/jsp/wm/repository/AdminRepository.java
@@ -1,5 +1,7 @@
 package com.jsp.wm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.wm.entity.Admin;
@@ -8,5 +10,7 @@ import com.jsp.wm.enums.AdminType;
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
 
 	public boolean existsByAdminType(AdminType adminType);
+
+	public Optional<Admin> findByEmail(String username);
 
 }
