abbd2b7869fe2bcfe110501c3bb7b1b63274af62
MohanKumarBT1997 <btmohankumar777@gmail.com>
2024-06-12 13:46:01 +0530

    Find Address

--- src/main/java/com/jsp/wm/controller/AddressController.java
+++ src/main/java/com/jsp/wm/controller/AddressController.java
@@ -3,6 +3,7 @@ package com.jsp.wm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jsp.wm.requestdto.AddressRequest;
 import com.jsp.wm.responsedto.AddressResponse;
+import com.jsp.wm.responsedto.AdminResponse;
 import com.jsp.wm.service.AddressService;
 import com.jsp.wm.utility.ResponseStructure;
 
@@ -37,5 +39,11 @@ public class AddressController {
 	public ResponseEntity<ResponseStructure<AddressResponse>> updateAddress(@RequestBody @Valid AddressRequest addressRequest,
 			@PathVariable int addressId){
 		return addressService.updateAddress(addressRequest,addressId);
+	}
+	
+	@PreAuthorize("hasAuthority('READ')")
+	@GetMapping("/addresses/{addressId}")
+	public ResponseEntity<ResponseStructure<AddressResponse>> findAddressById(@PathVariable int addressId){
+		return addressService.findAddressById(addressId);
 	}
 }
--- src/main/java/com/jsp/wm/serviceimpl/AddressServiceImpl.java
+++ src/main/java/com/jsp/wm/serviceimpl/AddressServiceImpl.java
@@ -13,6 +13,7 @@ import com.jsp.wm.repository.AddressRepository;
 import com.jsp.wm.repository.WareHouseRepository;
 import com.jsp.wm.requestdto.AddressRequest;
 import com.jsp.wm.responsedto.AddressResponse;
+import com.jsp.wm.responsedto.AdminResponse;
 import com.jsp.wm.service.AddressService;
 import com.jsp.wm.utility.ResponseStructure;
 
@@ -58,6 +59,20 @@ public class AddressServiceImpl implements AddressService{
 		return addressRepository.findById(addressId).map(exAddress -> {
 			addressMapper.mapToAddress(addressRequest, exAddress);
 			Address address = addressRepository.save(exAddress);
+			
+			return ResponseEntity.status(HttpStatus.OK).body(new ResponseStructure<AddressResponse>()
+					.setStatus(HttpStatus.OK.value())
+					.setMessage("Address updated").
+					setData(addressMapper.mapToAddressResponse(address))
+					);
+			
+		}).orElseThrow(() -> new AddressNotFoundByIdException("Address not found by Id"));
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<AddressResponse>> findAddressById(int addressId) {
+	
+		return addressRepository.findById(addressId).map(address -> {
 			
 			return ResponseEntity.status(HttpStatus.OK).body(new ResponseStructure<AddressResponse>()
 					.setStatus(HttpStatus.OK.value())
